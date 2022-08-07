8f8eb468d3ecde507bb13870374618e9ae2284a3
luwanqiang <luwanqiang>
2022-08-07 23:53:35 +0800

    备注

--- src/main/java/learn/Daemon.java
+++ src/main/java/learn/Daemon.java
@@ -6,7 +6,7 @@ import comm.SleepUtils;
  *  Java虚拟机退出时 Daemon线程中的 finally语句块不一定会执行：
  *      main线程在启动 daemonRunner线程后，daemonRunner线程随着main方法执行完毕而终止，
  *      此时JVM中已经没有 非Daemon线程，虚拟机需要退出，JVM中的所有 Daemon线程需要立即终止。
- *      因此 DaemonRunner线程中的 fainally语句块并没有执行
+ *      因此 DaemonRunner线程中的 fainally语句块并没有执行。
  */
 public class Daemon {
     public static void main(String[] args) {
@@ -26,6 +26,9 @@ public class Daemon {
             try {
                 SleepUtils.second(100);
             } finally {
+                /**
+                 *  在构建Daemon线程时，不能依靠 finally块中的内容来确保执行关闭或清理资源的逻辑
+                 */
                 System.out.println("DaemonThread finally run.");
             }
 
